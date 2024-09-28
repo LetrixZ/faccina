308f3c43f144ce91c6c521042c9f76602cbb07b3
Fermin Cirella <fermincirella.ar@gmail.com>
2024-09-28 15:42:41 -0300

    Remove siteConfig import

--- app/routes/+layout.svelte
+++ app/routes/+layout.svelte
@@ -1,13 +1,7 @@
 <script>
 	import { Toaster } from '$lib/components/ui/sonner';
 
-	import { siteConfig } from '~/lib/stores';
-
 	import '../app.pcss';
-
-	export let data;
-
-	$siteConfig = data.site;
 </script>
 
 <Toaster position="bottom-center" richColors />
