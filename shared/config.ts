380804f7c477697d93d887a4a40e306c34b5a360
Fermin Cirella <fermincirella.ar@gmail.com>
2024-10-04 01:35:39 -0300

    Update image-encoder

--- shared/config.ts
+++ shared/config.ts
@@ -68,32 +68,25 @@ const presetSchema = z
 			format: z.literal('webp'),
 			quality: z.number().min(1).max(100).optional(),
 			lossless: z.boolean().optional(),
-			near_lossless: z.boolean().optional(),
-			effort: z.number().min(0).max(6).optional(),
+			speed: z.number().min(0).max(6).optional(),
 		}),
 		z.object({
 			format: z.literal('jpeg'),
 			quality: z.number().min(1).max(100).optional(),
-			progressive: z.boolean().optional(),
 		}),
 		z.object({
 			format: z.literal('png'),
-			progressive: z.boolean().optional(),
-			effort: z.number().min(1).max(10).optional(),
-			compression_level: z.number().min(0).max(9).optional(),
 		}),
 		z.object({
 			format: z.literal('jxl'),
 			quality: z.number().min(1).max(100).optional(),
+			speed: z.number().min(3).max(9).optional(),
 			lossless: z.boolean().optional(),
-			effort: z.number().min(3).max(9).optional(),
-			distance: z.number().min(0).max(15).optional(),
 		}),
 		z.object({
 			format: z.literal('avif'),
 			quality: z.number().min(1).max(100).optional(),
-			lossless: z.boolean().optional(),
-			effort: z.number().min(0).max(9).optional(),
+			speed: z.number().min(1).max(10).optional(),
 		}),
 	])
 	.transform(camelize)
