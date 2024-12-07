7482fdb66918c8fd865bcab3a7122ab5760020e3
Fermin Cirella <fermincirella.ar@gmail.com>
2024-12-07 17:30:41 -0300

    - API: improve reader config

--- app/routes/api/config/+server.ts
+++ app/routes/api/config/+server.ts
@@ -3,9 +3,13 @@ import config from '~shared/config';
 
 export const GET = () => {
 	return json({
-		readerPresets: config.image.readerPresets.map((preset) => ({
-			name: preset.name,
-			label: preset.label,
-		})),
+		reader: {
+			presets: config.image.readerPresets.map((preset) => ({
+				name: preset.name,
+				label: preset.label,
+			})),
+			defaultPreset: config.image.readerDefaultPreset,
+			allowOriginal: config.image.readerAllowOriginal,
+		},
 	});
 };
